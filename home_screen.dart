8bed5806db37e3a3e8c0646f71b673ff85d2af5a
Odenfis <105258251+Odenfis@users.noreply.github.com>
2023-05-23 10:02:54 -0500

    Update home_screen.dart
    version2

--- home_screen.dart
+++ home_screen.dart
@@ -1,25 +1,33 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget{
-
   const HomeScreen({super.key});
-
   @override
   Widget build(BuildContext context) {
-
+    const fontSizeVar28 = TextStyle(fontSize: 28);
     return  Scaffold(
       appBar: AppBar(
-        title: const Text('Barra Superior'),
+        title: const Text('First App'),
+        backgroundColor: Colors.green.shade900,
         elevation: 12.5,
-      ),
-      
-      backgroundColor: Colors.grey,
+      ),      
+      backgroundColor: Colors.greenAccent,
       body: const Center(
-        child: Text('Hola Mundo V2'),
-
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          //crossAxisAlignment: CrossAxisAlignment.center,        
+          children: <Widget>[
+            Text('Contador de números con clic', style: fontSizeVar28,),
+            Text('60',style: fontSizeVar28,)            
+          ],
+        )
         ),
-    );
-  
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.upload),
+          onPressed: (){
+            print('Mensaje en consola');
+          }
+          ),
+    );  
   }
-
 }
