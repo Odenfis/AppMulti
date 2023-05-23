de321569e15f0bddca064ffb25b6c549620701ee
Odenfis <105258251+Odenfis@users.noreply.github.com>
2023-05-23 08:34:31 -0500

    Update home_screen.dart
    Version Corregida

--- home_screen.dart
+++ home_screen.dart
@@ -1,19 +1,25 @@
-import 'package:app_prueba/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(const MyApp());  
-}
+class HomeScreen extends StatelessWidget{
 
-class MyApp extends StatelessWidget{
-  const MyApp({super.key});
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
 
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomeScreen()
+    return  Scaffold(
+      appBar: AppBar(
+        title: const Text('Barra Superior'),
+        elevation: 12.5,
+      ),
+      
+      backgroundColor: Colors.grey,
+      body: const Center(
+        child: Text('Hola Mundo V2'),
+
+        ),
     );
+  
   }
+
 }
