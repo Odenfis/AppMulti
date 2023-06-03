f471b06786e608022e2cadd35dc868177b488fba
Odenfis <105258251+Odenfis@users.noreply.github.com>
2023-06-03 18:13:01 -0500

    version1

---
+++ Modulo2/screens/listview1_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ListView1Screen extends StatelessWidget {
+  const ListView1Screen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('ListView1Screen'),
+      ),
+    );
+  }
+}
